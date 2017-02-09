e390310dac8429f743c2bc94fe7acbd6ae6be2c4
cuigw <cuigw0293@cairenhui.com>
2017-02-09 09:05:36 +0800

    文件修改提交

--- src/main/java/com/vote/web/VoteController.java
+++ src/main/java/com/vote/web/VoteController.java
@@ -5,15 +5,16 @@ import com.vote.domain.BaseVoteMapper;
 import com.vote.result.SisapResult;
 import com.vote.util.MapUtil;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * 功能说明: 投票控制器<br>
@@ -31,8 +32,8 @@ public class VoteController {
     BaseVoteMapper baseVoteMapper;
 
     @ApiOperation(value="根据商户号获取所有投票对象", notes="根据商户号获取所有投票对象")
-    @RequestMapping(value="/business/{business_id}", method= RequestMethod.POST)
-    public SisapResult getVoteByBusinessId(@PathVariable String business_id) {
+    @RequestMapping(value="/business/", method= RequestMethod.POST)
+    public SisapResult getVoteByBusinessId(@RequestParam String business_id) {
         SisapResult sisapResult = new SisapResult("0", "查询成功!");
         try {
             List<BaseVote> baseVotes = baseVoteMapper.findByBusinessId(business_id);
@@ -58,9 +59,16 @@ public class VoteController {
 
     @ApiOperation(value="创建投票对象", notes="根据BaseVote对象创建")
     @RequestMapping(value="/voteAdd/", method=RequestMethod.POST)
-    public SisapResult postVote(@ModelAttribute BaseVote baseVote) {
+    public SisapResult postVote(@ModelAttribute BaseVote baseVote, @RequestParam(required = false) MultipartFile img_file) {
         SisapResult sisapResult = new SisapResult("0", "添加成功!");
         try {
+            //添加图片
+            if(null != img_file && StringUtils.isNotBlank(img_file.getOriginalFilename())) {
+                //转为图片路径
+                String img_path = handleFileUpload(img_file, 100l);
+                baseVote.setImg_url(img_path);
+            }
+
             int i = baseVoteMapper.insert(baseVote);
             if (i <= 0) {
                 sisapResult.setError_no("1");
@@ -75,8 +83,8 @@ public class VoteController {
     }
 
     @ApiOperation(value="更新投票对象详细信息", notes="根据url的vote_id来指定更新对象，并根据传过来的vote信息来更新投票对象详细信息")
-    @RequestMapping(value="/voteUpdate/{vote_id}", method=RequestMethod.POST)
-    public SisapResult putVote(@PathVariable Long vote_id, @ModelAttribute BaseVote baseVote) {
+    @RequestMapping(value="/voteUpdate/", method=RequestMethod.POST)
+    public SisapResult putVote(@RequestParam Long vote_id, @ModelAttribute BaseVote baseVote, @RequestParam(required = false) MultipartFile img_file) {
         SisapResult sisapResult = new SisapResult("0", "更新成功!");
         try {
             BaseVote baseVoteQuery = baseVoteMapper.query(vote_id);
@@ -84,6 +92,14 @@ public class VoteController {
                 sisapResult.setError_no("2");
                 sisapResult.setError_info("投票对象不存在！");
             }
+
+            //修改图片
+            if(null != img_file && StringUtils.isNotBlank(img_file.getOriginalFilename())) {
+                //转为图片路径
+                String img_path = handleFileUpload(img_file, baseVote.getVote_id());
+                baseVote.setImg_url(img_path);
+            }
+
             baseVoteMapper.update(baseVote);
         } catch (Exception e) {
             logger.error("（VoteController.putVote）更新投票对象异常", e );
@@ -94,8 +110,8 @@ public class VoteController {
     }
 
     @ApiOperation(value="获取投票对象详细信息", notes="根据url的vote_id来获取投票对象的详细信息")
-    @RequestMapping(value="/voteGet/{vote_id}", method=RequestMethod.POST)
-    public SisapResult getVote(@PathVariable Long vote_id) {
+    @RequestMapping(value="/voteGet/", method=RequestMethod.POST)
+    public SisapResult getVote(@RequestParam Long vote_id) {
         SisapResult sisapResult = new SisapResult("0", "查询成功!");
         try {
             BaseVote baseVoteQuery = baseVoteMapper.query(vote_id);
@@ -114,8 +130,8 @@ public class VoteController {
     }
 
     @ApiOperation(value="删除投票对象", notes="根据url的vote_id来指定删除投票对象")
-    @RequestMapping(value="/voteDelete/{vote_id}", method=RequestMethod.POST)
-    public SisapResult deleteVote(@PathVariable Long vote_id) {
+    @RequestMapping(value="/voteDelete/", method=RequestMethod.POST)
+    public SisapResult deleteVote(@RequestParam Long vote_id) {
         SisapResult sisapResult = new SisapResult("0", "删除成功!");
         try {
             BaseVote baseVoteQuery = baseVoteMapper.query(vote_id);
@@ -134,8 +150,8 @@ public class VoteController {
     }
 
     @ApiOperation(value="增加票数（投票）", notes="根据url的vote_id来指定更新票数")
-    @RequestMapping(value="/voteIncrease/{vote_id}", method=RequestMethod.POST)
-    public SisapResult voteIncrease(@PathVariable Long vote_id) {
+    @RequestMapping(value="/voteIncrease/", method=RequestMethod.POST)
+    public SisapResult voteIncrease(@RequestParam Long vote_id) {
         SisapResult sisapResult = new SisapResult("0", "投票成功!");
         try {
             BaseVote baseVoteQuery = baseVoteMapper.query(vote_id);
@@ -150,5 +166,42 @@ public class VoteController {
             sisapResult.setError_info("投票异常,请稍后重试！");
         }
         return sisapResult;
+    }
+
+    public String handleFileUpload(MultipartFile mfile, Long vote_id) throws Exception {
+
+        if (!mfile.isEmpty()) {
+            String filename = mfile.getOriginalFilename();
+            String file_extname = filename.substring(filename.lastIndexOf("."), filename.length());
+            filename = vote_id + "_" +  new Date().getTime();
+            String path = getRuntimeConfigPath() + File.separator + filename + file_extname;
+            try {
+                mfile.transferTo(new File(path));
+                path =  "http://139.129.47.102/" + filename + file_extname;        //转为http地址
+                return path;
+            } catch (IllegalStateException | IOException e) {
+                logger.error(e.getMessage());
+                throw new Exception(e);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取系统运行期配置文件路径
+     * @return
+     */
+    public static String getRuntimeConfigPath() {
+        String path = System.getenv().get("RUNTIME_CONFIG_ROOT");
+        if (StringUtils.isBlank(path)) {
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.indexOf("win") >= 0) {
+                path = "E:"+File.separator+"votefile";
+            }
+            else {
+                path = "/usr/cgw/votefile";
+            }
+        }
+        return path;
     }
 }
